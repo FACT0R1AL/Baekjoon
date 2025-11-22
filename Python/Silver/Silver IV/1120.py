06eaf1cfe7bd7ff296e013ee835a099b5c5845e8
FACT0R1AL <factorial4861@gmail.com>
2025-11-22 18:08:35 +0900

    solved 1120

--- Python/Silver/Silver IV/1120.py
+++ Python/Silver/Silver IV/1120.py
@@ -3,3 +3,15 @@ word1, word2 = input().split()
 lengthA = len(word1)
 lengthB = len(word2)
 
+minCount = lengthB
+
+for i in range(lengthB - lengthA + 1):
+    count = 0
+    temp = word2[i:i+lengthA]
+
+    for j in range(lengthA):
+        if (word1[j] != temp[j]): count += 1
+
+    if (count < minCount): minCount = count
+
+print(minCount)

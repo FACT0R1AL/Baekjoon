638d619cc3ea1b40fa6fafbaff6c8a9397714745
FACT0R1AL <factorial4861@gmail.com>
2025-11-25 22:56:19 +0900

    solved 1456

--- Python/Gold/Gold V/1456_nc.py
+++ Python/Gold/Gold V/1456_nc.py
@@ -18,43 +18,24 @@ for i in range(len(primes)):
 
 almost_primes.sort()
 
-def binary_search(arr, target, mode):
-    left, right = 0, len(arr) - 1
-    result = -1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if mode == 0:
-            if arr[mid] <= target:
-                result = mid
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        else:
-            if arr[mid] >= target:
-                result = mid
-                right = mid - 1
-            else:
-                left = mid + 1
-
-    if result == -1:
-        return 0 if mode == 0 else len(arr)
-
-    return result
-
 a, b = map(int, input().split())
 
-idxLeft = binary_search(almost_primes, a, 0)
-idxRight = binary_search(almost_primes, b, 1)
+left, right = 0, len(almost_primes)
 
-print(idxRight, len(almost_primes))
-count = len(almost_primes[idxLeft+1:idxRight])
+while left <= right:
+    mid = (left + right) // 2
+    if almost_primes[mid] < a:
+        left = mid + 1
+    else:
+        right = mid - 1
 
-if (almost_primes[idxLeft] == a): count += 1
-if (idxRight != len(almost_primes) and almost_primes[idxRight] == b): 
-    if (a != b): count += 1
-if (a == 1): count += 1
-print(almost_primes[idxLeft+1:idxRight])
+idx = mid
+count = 0
+
+while idx < len(almost_primes) and almost_primes[idx] <= b:
+    # print(almost_primes[idx])
+
+    if (almost_primes[idx] >= a): count += 1
+    idx += 1
+
 print(count)

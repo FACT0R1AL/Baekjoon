bdff1d5e8a5b91ac3bfa81526d96eae4fd789fd7
FACT0R1AL <factorial4861@gmail.com>
2026-03-04 20:01:38 +0900

    update 5615

--- Python/Platinum/Platinum I/5615.py
+++ Python/Platinum/Platinum I/5615.py
@@ -2,46 +2,39 @@ import sys
 
 input = sys.stdin.readline
 
-def fastPow(base, exp, mod):
-    result = 1
-    base %= mod
-
-    while exp > 0:
-        if (exp%2):
-            result = (result*base) % mod
+def check(a, d, n, s):
+    x = pow(a, d, n)
+    if x == 1 or x == n - 1:
+        return True
         
-        base = (base*base) % mod
-        exp //= 2
+    for _ in range(s - 1):
+        x = pow(x, 2, n)
 
-    return result
+        if x == n - 1:
+            return True
+            
+    return False
 
-def milLaPrime(n):
-    tempA = [2, 7, 61]
+def isPrime(n):
+    if n < 2:
+        return False
+
+    testPrimes = [2, 3, 5, 7, 11]
+
+    # n-1 = d * 2^s 분해
+    d = n - 1
     s = 0
-    p = n - 1
-
-    while p % 2 == 0:
-        p //= 2
+    while d % 2 == 0:
+        d //= 2
         s += 1
-    d = p
 
-    for a in tempA:
-        if a % n == 0: continue
-        x = fastPow(a, d, n)
+    for a in testPrimes:
+        if a % n == 0:
+            continue
+        if not check(a, d, n, s):
+            return False
 
-        if x == 1 or x == n - 1: continue
-
-        flag = False
-        for _ in range(s - 1):
-            x = fastPow(x, 2, n)
-            if x == n - 1:
-                flag = True
-                break
-
-        if not flag: return 0
-
-    return 1
-
+    return True
 count = 0
 
 T = int(input().rstrip())
@@ -49,6 +42,6 @@ T = int(input().rstrip())
 for _ in range(T):
     n = int(input().rstrip())
     
-    if (milLaPrime(2*n+1)): count += 1
+    if (isPrime(2*n+1)): count += 1
 
 print(count)
